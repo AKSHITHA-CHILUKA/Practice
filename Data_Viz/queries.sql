f1d1bbdd2bb7821e9f9aa9087a00e0b86213efc9
~Chiluka Akshitha <22wh1a12b5@bvrithyderabad.edu.in>
2026-07-06 20:32:32 +0530

    Create queries.sql

---
+++ Data_Viz/queries.sql
@@ -0,0 +1,8 @@
+--database:mysql
+
+-- This is a sample SQL query file for data visualization purposes.
+-- get all users from the users table
+SELECT * FROM users;
+
+-- get all users from users table with name 'john'
+SELECT * FROM users WHERE name = 'john';
